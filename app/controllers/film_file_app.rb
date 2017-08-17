cd4a71bf64d36e446d216ff0fa8b6dbf9248c0e2
Daniel Phillips <danieledward.phillips@gmail.com>
2017-08-17 09:37:37 -0600

    commit it

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,20 @@
 class FilmFile < Sinatra::Base
+
   get '/films' do
+    @films = Film.all
     erb :films_index
   end
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :genres_index
+  end
+
+  get '/directors/:id' do
+    require 'pry';binding.pry
+    @director = Director.find(params[:id])
+    erb :director_films_index
+  end
+
+
 end
